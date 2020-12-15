66ec70a7f9eea32e9203cc1744eb76b439b2b51c
DESKTOP-T1FEPOR\Filip <filip.wiatrowski@gmail.com>
2020-12-15 18:32:01 +0100

    Created sitemap.xml and url path. Created RSS feed.

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+SITE_ID = 1
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -47,7 +48,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
-    'taggit'
+    'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps'
 ]
 
 MIDDLEWARE = [
